121d80d110ce8538150ad5c71673c0d9d0493b4d
Akshay <akshay@workplacefabric.com>
2021-06-27 14:12:09 +0530

    setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const forecast = require('./utils/forecast.js')
 
 const app=express()
 
+const port = process.env.PORT || 3000
+
 //define paths for express config
 const publicDirectoryPath = path.join(__dirname,'../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -101,6 +103,6 @@ app.get('*', (req,res)=>{
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server running on 3000')
+app.listen(port, () => {
+    console.log('Server running on '+port)
 })
